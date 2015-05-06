1e6d8f58be7d2aef037bd6805c8e3bad95acb3fe
mzxrules <mzxrules@gmail.com>
2015-05-06 18:43:21 -0400

    More actor doc

--- ZActor/OActors/TransitionPlaneActor.cs
+++ ZActor/OActors/TransitionPlaneActor.cs
@@ -8,10 +8,13 @@ namespace ZActor.OActors
 {
     class TransitionPlaneActor : TransitionActor
     {
+        SwitchFlag flag;
+        ushort type;
         public TransitionPlaneActor(byte[] record)
             : base(record)
         {
-
+            flag = Pack.AsByte(Variable, 0x003F);
+            type = Pack.AsUInt16(Variable, 0xFFC0);
         }
         //public override string Print()
         //{
@@ -22,6 +25,10 @@ namespace ZActor.OActors
         protected override string GetActorName()
         {
             return "Transition Plane";
+        }
+        protected override string GetVariable()
+        {
+            return string.Format("{2} Type {0}: {1}", type, flag, this.PrintTransition());
         }
     }
 }
